cd37e68dc993fbb2a8d228b0e5e661eece4c31b2
Dennis Prazak <prazak.dennis@gmail.com>
2024-05-23 13:29:09 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+﻿This repo replicated the following bug https://github.com/typestack/class-transformer/issues/1446
+
+# Steps
+1. `pnpm dev`
+2. Open devtools console in the browser
+3. Observe
+```
+Person { numbers: Set { 1, 2, 3, 4, 5 } }
+{ numbers: [ 1, 2, 3, 4, 5 ] }
+Person { numbers: [ 1, 2, 3, 4, 5 ] }
+```
+
+when we expected
+```
+Person { numbers: Set { 1, 2, 3, 4, 5 } }
+{ numbers: [ 1, 2, 3, 4, 5 ] }
+Person { numbers: Set { 1, 2, 3, 4, 5 } }
+```
+
+**Place of interest: ./src/App.tsx**
---
+++ src/App.tsx
@@ -0,0 +1,38 @@
+﻿import 'reflect-metadata'
+import { Type, instanceToPlain, plainToInstance } from 'class-transformer'
+import { useState, useEffect } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+class Person {
+    @Type(() => Number)
+    public numbers: Set<number> = new Set()
+}
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  useEffect(() => {
+    const person = new Person()
+    person.numbers = new Set([1,2,3,4,5])
+    const plain = instanceToPlain(person)
+    const instance = plainToInstance(Person, plain)
+
+    console.log("========")
+    console.log(person)
+    console.log(plain)
+    console.log(instance)
+    console.log("========")
+  }, [])
+  
+  return (
+    <>
+      <div>
+            <p>Hello, World</p>
+      </div>
+    </>
+  )
+}
+
+export default App
